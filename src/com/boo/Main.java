233376f57c2dfecd4f7ba2ded3f213edbe268d8c
dmazaraki <dimitramaz@yahoo.gr>
2019-02-07 15:56:11 +0100

    "3nd commit"

--- src/com/boo/Main.java
+++ src/com/boo/Main.java
@@ -3,6 +3,30 @@ package com.boo;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+       /* Player player = new Player();
+        player.name = "boo";
+        player.health= 20;
+        player.weapon = "sword";
+
+        int damage = 10;
+        player.loseHealth(damage);
+        System.out.println("Remaining health = "
+                + player.healthRemaining());
+        System.out.println();
+        damage = 11;
+        //player.health = 200;
+        player.loseHealth(damage);
+        System.out.println("Remaining health = "
+                + player.healthRemaining() + "\n");
+*/
+        //
+        EnhancedPlayer enhancedPlayer = new
+                EnhancedPlayer("boo", 240,
+                "ninjutsu");
+        System.out.println("Initial health is " + enhancedPlayer.getHealth() );
+        enhancedPlayer.loseHealth(10);
+
     }
+
+
 }
