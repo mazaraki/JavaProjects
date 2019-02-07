1b09d2637e8cd180928ccfaf84ee6e1bdbf13c85
dmazaraki <dimitramaz@yahoo.gr>
2019-02-07 16:39:28 +0100

    "2nd commit"

--- src/com/boo/Main.java
+++ src/com/boo/Main.java
@@ -3,6 +3,11 @@ package com.boo;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Printer printer = new Printer(30,20, true);
+
+        printer.pages(5, 10);
+
+        printer.tonerLevelFilled(50);
+        printer.tonerLevelFilled(80);
     }
 }
---
+++ src/com/boo/Printer.java
@@ -0,0 +1,56 @@
+package com.boo;
+
+public class Printer {
+    private int tonerLevel;
+    private int pagesPrinted;
+    private boolean isDuplex;
+
+    public Printer(int tonerLevel,
+                   int pagesPrinted,
+                   boolean isDuplex) {
+        this.tonerLevel = tonerLevel;
+        this.pagesPrinted = pagesPrinted;
+        this.isDuplex = isDuplex;
+    }
+
+    public Printer(int pagesPrinted,
+                   boolean isDuplex) {
+        this(100, pagesPrinted, isDuplex );
+    }
+
+    public void tonerLevelFilled(int num){
+        int total = num + getTonerLevel();
+
+        if (total > 99){
+            System.out.println("Printer is completely filled");
+        } else if (total < 100 && total > 0){
+            System.out.println("Printer is filled " + total + "%");
+        } else {
+            System.out.println("An error occurred.");
+        }
+    }
+
+    public int pages(int initial, int times){
+        if (isDuplex){
+            pagesPrinted = (initial * times) / 2;
+        } else {
+            pagesPrinted = initial * times;
+        }
+        System.out.println("pages printed : " + pagesPrinted);
+        return pagesPrinted;
+    }
+
+    public int getTonerLevel() {
+        int p = getPagesPrinted();
+        tonerLevel -= (p * .01);
+        return tonerLevel;
+    }
+
+    public int getPagesPrinted() {
+        return pagesPrinted;
+    }
+
+    public boolean isDuplex() {
+        return isDuplex;
+    }
+}
