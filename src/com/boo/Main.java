114062af1e8898328e32112200f72d42ca256e68
dmazaraki <dimitramaz@yahoo.gr>
2019-02-12 14:40:09 +0100

    "Initial commit"

---
+++ src/com/boo/Main.java
@@ -0,0 +1,10 @@
+package com.boo;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int myVar = 50;
+        int[] myIntArr = new int[10];
+        myIntArr[5] = 50;
+    }
+}
