8077c78b685c7e9dd01b4ca598f946bdf72d47f1
miminaki <dimitramaz@yahoo.gr>
2019-02-20 15:09:01 +0100

    "2nd commit"

--- src/com/boo/Main.java
+++ src/com/boo/Main.java
@@ -1,8 +1,35 @@
 package com.boo;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
 
+    private static final Scanner scanner = new Scanner(System.in);
+    private static int[] array;
+
     public static void main(String[] args) {
-	// write your code here
+
+        System.out.println("How many numbers are you entering?");
+        int n = scanner.nextInt();
+        array = new int[n];
+        for (int i = 0; i < n; i++){
+            array[i] = scanner.nextInt();
+        }
+        scanner.nextLine();
+        reverse(array);
+    }
+
+    public static void reverse(int[] array){
+        System.out.println("Array before reverse " + Arrays.toString(array));
+        int temp, count = 0;
+        for (int i = array.length-1; i > array.length / 2; i--){
+            temp = array[i];
+            array[i] =  array[count];
+            array[count] = temp;
+            count++;
+        }
+
+        System.out.println("Reversed array : " + Arrays.toString(array));
     }
 }
